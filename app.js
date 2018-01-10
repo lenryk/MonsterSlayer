20ba1d719179e98676ed9c7365a5e797c3cd5090
lenryk <lenry101@gmail.com>
2018-01-10 20:39:45 +0000

    Finished project in vueJS

---
+++ app.js
@@ -0,0 +1,95 @@
+new Vue({
+    el: "#app",
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns: []
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack: function() {
+            let damage = this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+               isPlayer: true,
+               text: "Player hits Monster for " + damage
+            });
+            if(this.checkWin()) {
+                return;
+            }
+
+           this.monsterAttacks();
+        },
+        specialAttack: function() {
+            let damage = this.calculateDamage(10,20);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Player hits the monster HARD for " + damage
+            });
+
+            if(this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+
+        },
+        heal: function() {
+
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Players heals for 10"
+            });
+
+            this.monsterAttacks();
+        },
+        giveUp: function() {
+            this.gameIsRunning = false;
+        },
+        monsterAttacks: function() {
+            let damage = this.calculateDamage(5,12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: "Monster hits Player for " +damage
+            })
+        },
+        calculateDamage: function(min,max) {
+            return Math.floor((Math.random() * max) + min);
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                this.monsterHealth = 0;
+                if(confirm("You won! New game?")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                this.playerHealth = 0;
+                if(confirm("You lost! New game?")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+});
